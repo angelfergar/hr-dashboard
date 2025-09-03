0cdc4b247dcce6e429c5f4b6117ad55c8763124d
angelfergar <anfernagar@gmail.com>
2025-09-03 12:16:30 +0200

    Fix issue with DOB date format

--- sql-queries/format-columns.sql
+++ sql-queries/format-columns.sql
@@ -8,10 +8,14 @@ ALTER TABLE `hr-project-470820.HR_Data.RawData` ADD COLUMN ManagerID_format INT;
 -- Format the dates
 UPDATE `hr-project-470820.HR_Data.RawData`
 SET 
-DOB_format = PARSE_DATE('%m/%d/%Y ', DOB),
+DOB_format = DATE(
+          CAST(SPLIT(DOB, '/')[OFFSET(2)] AS INT64) + 1900,  -- Adjust year
+      CAST(SPLIT(DOB, '/')[OFFSET(0)] AS INT64),         
+      CAST(SPLIT(DOB, '/')[OFFSET(1)] AS INT64)         
+),
 DateofHire_format = PARSE_DATE('%m/%d/%Y', DateofHire),
-DateofTermination_format = PARSE_DATE('%m/%d/%Y ', DateofTermination),
-LastPerformanceReview_Date_format = PARSE_DATE('%m/%d/%Y ', LastPerformanceReview_Date),
+DateofTermination_format = PARSE_DATE('%m/%d/%Y', DateofTermination),
+LastPerformanceReview_Date_format = PARSE_DATE('%m/%d/%Y', LastPerformanceReview_Date),
 ManagerID_format = CAST(ManagerID AS INT)
 WHERE TRUE;
 
